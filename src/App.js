966018e2e4fa712a56a2e206ff7f55c44c38d5e3
Adam Austin <aausti23@myune.edu.au>
2020-09-15 22:19:48 +1000

    Card data added

--- src/App.js
+++ src/App.js
@@ -1,15 +1,30 @@
 import React from 'react';
 
 import {Cards,Chart, CountryPicker} from './components';
+import {fetchData} from './api';
+
+import styles from './App.module.css';
 
 class App extends React.Component {
+    state = {
+        data: {},
+    }
+
+
+    async componentDidMount(){
+        const data = await fetchData();
+        this.setState({data:data});
+    }
+
+
+
     render(){
+        const {data} = this.state;
         return(
-            <div>
-                <h1>App</h1>
-                <Cards />
-                <Chart />
+            <div className={styles.container}>
+                <Cards data={data}/>
                 <CountryPicker />
+                <Chart />
             </div>
         )
     }
--- src/components/Cards/Cards.jsx
+++ src/components/Cards/Cards.jsx
@@ -1,8 +1,66 @@
 import React from 'react';
+import {Card, CardContent, Typography, Grid} from '@material-ui/core';
+import CountUp from 'react-countup';
 
-const Cards = () =>{
+import styles from './Cards.module.css';
+
+const Cards = ({data: {confirmed, recovered, deaths, lastUpdate}}) =>{
+    if(!confirmed){
+        return 'Loading...';
+    }
+
     return (
-        <h1>Cards</h1>
+        <div className={styles.container}>
+            <Grid container spaceing={3} justify="center">
+                <Grid item component={Card}>
+                    <CardContent>
+                        <Typography color="textSecondary" gutterBottom>Infected</Typography>
+                        <Typography variant="h5">
+                            <CountUp 
+                                start={0}
+                                end={confirmed.value}
+                                duration={2.5}
+                                separator=","
+                            />
+                        </Typography>
+                        <Typography color="textSecondary">{new Date(lastUpdate).toDateString()}</Typography>
+                        <Typography variant="body2">Number of Active Cases of Covid 19</Typography>
+                    </CardContent>
+                </Grid>
+
+                <Grid item component={Card}>
+                    <CardContent>
+                        <Typography color="textSecondary" gutterBottom>Recovered</Typography>
+                        <Typography variant="h5">
+                            <CountUp 
+                                start={0}
+                                end={recovered.value}
+                                duration={2.5}
+                                separator=","
+                            />
+                        </Typography>
+                        <Typography color="textSecondary">{new Date(lastUpdate).toDateString()}</Typography>
+                        <Typography variant="body2">Number of Recoveries From of Covid 19</Typography>
+                    </CardContent>
+                </Grid>
+
+                <Grid item component={Card}>
+                    <CardContent>
+                        <Typography color="textSecondary" gutterBottom>Deaths</Typography>
+                        <Typography variant="h5">
+                            <CountUp 
+                                start={0}
+                                end={deaths.value}
+                                duration={2.5}
+                                separator=","
+                            />
+                        </Typography>
+                        <Typography color="textSecondary">{new Date(lastUpdate).toDateString()}</Typography>
+                        <Typography variant="body2">Number of Deaths From of Covid 19</Typography>
+                    </CardContent>
+                </Grid>
+            </Grid>
+        </div>
     )
 }
 
